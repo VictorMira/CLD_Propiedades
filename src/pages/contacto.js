5a474046a851ce4bf1013a7d55bb02cf957d9b76
Victor Mira <38212360+VictorMira@users.noreply.github.com>
2020-05-22 01:24:08 -0400

    fix email de contacto

--- src/pages/contacto.js
+++ src/pages/contacto.js
@@ -61,7 +61,7 @@ export default () => (
             <p>Contáctenos para solicitar una visita a la propiedad seleccionada</p>
 
             <p><FaPhone/> +569 8691 1018</p> 
-            <a href="mailto: corredora@latorredecizer.com"><p><FaEnvelope /> corredora@gvvpropiedades.com</p></a>
+            <a href="mailto: corredora@latorredecizer.com"><p><FaEnvelope /> corredora@latorredecizer.com</p></a>
             <p><FaMapMarked /> Arturo Prat 199, Piso 16, Concepción</p> 
             </div>
         </div>
